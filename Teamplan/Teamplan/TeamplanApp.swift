1f47fdfa0170917386dfabb6b91ac49ea5672e6d
Brigitte Michau <brigitte.michau@mooncascade.com>
2025-05-02 12:17:50 +0300

    Initial Xcode workspace and project setup

---
+++ Teamplan/Teamplan/TeamplanApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamplanApp.swift
+//  Teamplan
+//
+//  Created by Brigitte Michau on 02.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct TeamplanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Teamplan/TeamplanTests/TeamplanTests.swift
@@ -0,0 +1,17 @@
+//
+//  TeamplanTests.swift
+//  TeamplanTests
+//
+//  Created by Brigitte Michau on 02.05.2025.
+//
+
+import Testing
+@testable import Teamplan
+
+struct TeamplanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
